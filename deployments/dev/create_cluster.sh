9de411ac7d78f957747ca6868553162e8decfe80
satejchaudhary <chaudharysatej@gmail.com>
2025-05-16 12:13:13 -0700

    Tested Kafka and pushed fake data to kafka.

--- deployments/dev/create_cluster.sh
+++ deployments/dev/create_cluster.sh
@@ -14,6 +14,7 @@ kubectl create namespace kafka
 kubectl create -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
 kubectl apply -f manifests/kafka-e11b.yaml
 
+
 # Install kafka-ui
 kubectl apply -f manifests/kafka-ui-all-in-one.yaml
 # Kafka UI is now running on port 8080 of the kubernates cluster
@@ -23,3 +24,15 @@ echo "Kafka UI is running at http://localhost:8182"
 
 
 
+# Port forward the following services to our local machine. 
+# Option 1: Use Rancher Desktop
+# Rancher Desktop > Port Forwarding
+# kafka-ui-port=8182
+
+# Running kubectl port-forward from the terminal also  works but needs to be redone every time the laptop sleeps and we restart.
+
+kubectl port-forward -n kafka svc/kafka-ui 8182:8080 &
+echo "Kafka UI is running at http://localhost:8182"
+
+kubectl port-forward -n kafka service/kafka-e11b-kafka-bootstrap 31092:31092 &
+echo "Kafka is running at localhost:31092"
